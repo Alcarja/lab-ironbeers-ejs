d038b7a6b5f27e74fcc8fa6b5ea970b03bec9da0
Alcaraz <jaime.mediano@gmail.com>
2023-07-04 15:53:11 +0200

    Finished it, now styling

--- app.js
+++ app.js
@@ -19,4 +19,20 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => res.render('beers', {data: beersFromApi})) //As soon as getBeers works we render the page 'beers'. We change beersFromApi into an object so we can filter using the key and values.
+  .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    res.render('randomBeer', {randomBeer: responseFromAPI[0]});
+  })
+  .catch(error => console.log(error));
+})
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
